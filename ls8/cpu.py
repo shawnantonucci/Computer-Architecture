c6318f40d4bffaa97223c2b84adf04d27554cce3
shawn <shawnantonucci2018@gmail.com>
2019-07-23 17:14:02 -0700

    added step 9

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,68 +7,93 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        self.memory = [0] * 256
         self.register = [0] * 8
-        self.IR = 0
-        self.PC = 0
-        self.MDR = 0
-        self.MAR = 0
+        self.ram = [0] * 256
+        self.pc = 0
 
-    def ram_read(self, MAR):
-        return self.memory[MAR]
+        self.branchtable = {}
+        self.branchtable[int(0b10100000)] = self.handle_ADD
+        self.branchtable[int(0b10100011)] = self.handle_SUB
+        self.branchtable[int(0b10100010)] = self.handle_MUL
+        self.branchtable[int(0b10100011)] = self.handle_DIV
+        # self.branchtable[int(0b01001000)] = self.handle_PRA
+        self.branchtable[int(0b01000111)] = self.handle_PRN
+        self.branchtable[int(0b10000010)] = self.handle_LDI
+        # self.branchtable[int(0b00000001)] = self.handle_HLT
 
-    def ram_write(self, MDR, MAR):
-        self.memory[MAR] = MDR
+    def handle_ADD(self, operand_a, operand_b):     #  ADD
+        self.alu("ADD", operand_a, operand_b)
+        self.pc += 3
+    def handle_SUB(self, operand_a, operand_b):     # Subtract
+        self.alu("SUB", operand_a, operand_b)
+        self.pc += 3
+    def handle_MUL(self, operand_a, operand_b):     # Multiply
+        self.alu("MUL", operand_a, operand_b)
+        self.pc += 3
+    def handle_DIV(self, operand_a, operand_b):     # Divide
+        self.alu("DIV", operand_a, operand_b)
+        self.pc += 3
+
+    # def handle_PRA(self, operand_a, operand_b):     #
+
+    def handle_PRN(self, operand_a, operand_b):     # Print number in register
+        print(f'{self.register[operand_a]}')
+        self.pc += 2
+    def handle_LDI(self, operand_a, operand_b):     # LDI:
+        self.register[operand_a] = operand_b
+        self.pc += 3
+
+    def dispatch(self, IR, opA, opB):
+        self.branchtable[IR](opA, opB)
 
     def load(self):
         """Load a program into memory."""
 
 
-        # For now, we've just hardcoded a program:
+
         if len(sys.argv) is not 2:
-            print(f"Sorry, no program found")
+            print(f"usage: {sys.argv[0]} <filename>")
             sys.exit(1)
+
         try:
             address = 0
             program_name = sys.argv[1]
-            with open(program_name) as file:
-                for line in file:
-                    number = line.split("#", 1)[0]
-                    if number.strip() == "":
+
+            with open(program_name) as f:
+                for line in f:
+                    num = line.split("#", 1)[0]
+
+                    if num.strip() == '':  # ignore comment-only lines
                         continue
-                    num = "0b" + number
-                    print(num)
-                    self.memory[address] = int(num, 2)
-                    address +=1
-                    # print(f"{self.memory}")
+                    num = '0b' + num
+                    # print(num)
+                    self.ram[address] = int(num, 2)
+                    address += 1
+
+            print(self.ram)
+
         except FileNotFoundError:
-            print("Sys.argv not found")
+            print(f"{sys.argv[0]}: {sys.argv[1]} not found")
             sys.exit(2)
 
-        # program = [
-        #     # From print8.ls8
-        #     0b10000010, # LDI R0,8
-        #     0b00000000,
-        #     0b00001000,
-        #     0b01000111, # PRN R0
-        #     0b00000000,
-        #     0b00000001, # HLT
-        # ]
+    def ram_read(self, address):
+        return self.ram[address]
 
-        # for instruction in program:
-        #     self.memory[address] = instruction
-        #     address += 1
-
-        print(f"{self.memory}")
+    def ram_write(self, value, address):
+        self.ram[address] = value
+        pass
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
-        if op == 0b10100000:
+        if op == "ADD":
             self.register[reg_a] += self.register[reg_b]
-        #elif op == "SUB": etc
-        elif op == 0b10100010:
+        elif op == "SUB":
+            self.register[reg_a] -= self.register[reg_b]
+        elif op == "MUL":
             self.register[reg_a] *= self.register[reg_b]
+        elif op == "DIV":
+            self.register[reg_a] /= self.register[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -96,32 +121,12 @@ class CPU:
         running = True
 
         while running:
-            self.IR = self.PC
-            operand_a = self.ram_read(self.IR + 1)
-            operand_b = self.ram_read(self.IR + 2)
-            # print("Loop")
 
-            if self.memory[self.IR] == 0b00000001:
+            IR = self.pc
+            operand_a = self.ram_read(IR + 1)
+            operand_b = self.ram_read(IR + 2)
+            if self.ram[IR] == int(0b00000001):               # HLT base case: exit loop
                 running = False
-
-            elif self.memory[self.IR] == 0b10000010:
-                self.register[operand_a] = operand_b
-                # print("LDI")
-                self.PC += 3
-
-            elif self.memory[self.IR] == 0b01000111:
-                print(self.register[operand_a])
-                # print("PRN")
-                self.PC += 2
-
-            elif self.memory[self.IR] == 0b10100010:
-                self.alu(0b10100010, operand_a, operand_b)
-                self.PC += 3
-
-# cpu = CPU()
-# # cpu.ram_write(20, 0)
-# print(cpu.memory)
-# # print(cpu.trace)
-# # print(cpu.ram_read(0))
-# cpu.load()
-# cpu.run()
+            else:
+                # print(self.ram[IR])
+                self.dispatch(self.ram[IR], operand_a, operand_b)
